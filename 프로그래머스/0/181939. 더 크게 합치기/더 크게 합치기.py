ff57d3e5add973ecaf4e2041875e30c4e9233c3b
2ㅣ2ㅣ <choisui821@gmail.com>
2025-01-12 16:53:13 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,5 +1,3 @@
 def solution(a, b):
-    res1, res2 =  '', ''
-    res1 = str(a) + str(b)
-    res2 = str(b) + str(a)
-    return int(res1) if int(res1) > int(res2) else int(res2)
+    a, b= str(a), str(b)
+    return int(max(a+b, b+a))
