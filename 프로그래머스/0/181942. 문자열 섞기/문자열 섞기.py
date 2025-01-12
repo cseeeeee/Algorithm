447b3baca55f6527f2f935bcd5a89bfc1c9eab50
2ㅣ2ㅣ <choisui821@gmail.com>
2025-01-12 16:20:23 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,13 +1,6 @@
 def solution(str1, str2):
-    temp = ""
-    for i in range(0, len(str1)+len(str2)):
-        if i % 2 == 0:
-            #str1에 접근
-            index_of_string = i // 2
-            temp += str1[index_of_string] 
-        else:
-            #str2에 접근
-            index_of_string=i//2
-            temp += str2[index_of_string] 
-    return temp
-            
+    ans = ''
+    for i in range(len(str1)):
+        ans += str1[i]
+        ans += str2[i]
+    return ans
