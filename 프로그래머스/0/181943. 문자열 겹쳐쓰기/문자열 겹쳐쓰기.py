28833a94a62b47d1e8cd9ab6ee50a5f93b9b8381
2ㅣ2ㅣ <choisui821@gmail.com>
2025-01-12 15:49:27 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,2 +1,6 @@
 def solution(my_string, overwrite_string, s):
-    return my_string[:s]+overwrite_string+my_string[len(overwrite_string)+s:]
+    answer = ''
+    answer = my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
+    return answer
+
+
