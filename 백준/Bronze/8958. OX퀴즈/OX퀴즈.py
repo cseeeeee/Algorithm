63c34c1a3dbf3394312177111f8104470756f9f9
2ㅣ2ㅣ <choisui821@gmail.com>
2025-02-12 12:23:56 +0900

    [Bronze II] Title: OX퀴즈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,10 +1,7 @@
 def xo_game(xo_str):
   sum, count = 0, 0
   for i in range(len(xo_str)):
-    if xo_str[i] != "X":
-      count += 1
-    else:
-      count = 0
+    count = count+1 if xo_str[i] != "X" else 0
     sum += count
   return sum
 
