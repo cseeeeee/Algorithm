7e1ce1e0fafa83097aad597fca7f3ed07f7f47d5
2ㅣ2ㅣ <choisui821@gmail.com>
2025-02-27 15:26:47 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
@@ -1,6 +1,7 @@
 def solution(numbers, n):
-    sum=0
-    for i in numbers:
-        sum+=i
-        if sum > n:
-            return sum
+    total = 0
+    for num in numbers:
+        total += num
+        if total > n:
+            break
+    return total
